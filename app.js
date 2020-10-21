acbe2cfaafe5877ca0f7714d6b1d44e434c9e239
dev348 <mswaran348@gmail.com>
2020-10-21 22:45:01 +0530

    TODO

---
+++ app.js
@@ -0,0 +1,48 @@
+// This is the array that will hold the todo list items
+
+let todoInput = [];
+//function addtodo
+function addTodo(text){
+    const todo ={
+        text, id: Date.now(),
+    };
+
+    todoInput.push(todo);
+    // console.log(todoInput);
+
+}
+function actionTodo() {
+    
+
+    // select the text input
+    const input = document.getElementById('getTodo');
+
+    const text = input.value.trim();
+  if (text !== '') {
+    addTodo(text);
+    input.value = '';
+    input.focus();
+  }
+  for(let index = 0;index < todoInput.length;index++){
+    if(todoInput[index].text === text )
+       var displayValue= todoInput[index].text;
+  }
+
+   myDisplay(displayValue)
+
+}
+function myDisplay(value) {
+  var ul = document.createElement("ul");
+  var li = document.createElement("li");
+  var todoList= document.createElement("p");
+
+  var list = ul.innerText=(li);
+
+  li.innerText = (  todoList.innerText=(value));
+   
+ 
+ var viewOnsrceen= document.getElementById("displayTodo");
+ viewOnsrceen.appendChild(list);
+  //  return document.getElementById("displayTodo").innerHTML = (value);
+  }
+
